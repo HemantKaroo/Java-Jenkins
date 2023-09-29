aeb4e3801bb6260f86e94eecb1128ca69ebed72b
Hemant Karoo <hemantkaroohmk@gmail.com>
2023-09-29 17:05:10 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("This program for jenkins");
+        System.out.println("hello");
     }
 }
