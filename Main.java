3fc5ad07c14a64972a3f2492c9f38be6fd45bd0f
Hemant Karoo <hemantkaroohmk@gmail.com>
2023-09-30 14:27:24 +0530

    New file added

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
         System.out.println("This is java program");
         System.out.println("This program for jenkins");
         System.out.println("hello");
+        System.out.println("Added New File");
     }
 }
